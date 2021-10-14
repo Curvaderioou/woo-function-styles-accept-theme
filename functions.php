549a4a02539ce0bc89aca8ab929463359beef9f9
Curvaderioou <felipedegenerportella@yahoo.com>
2021-10-14 16:50:04 -0300

    retirado scripts de outras funções

--- functions.php
+++ functions.php
@@ -1,11 +1,5 @@
 <?php
 
-function handel_add_woocommerce_suport(){
-  add_theme_support('woocommerce');
-}
-add_action('after_setup_theme', 'handel_add_woocommerce_suport');
-
-
 /*
 function handel_css(){
   wp_register_style('handel-style', get_template_directory_uri() . '/style.css')
